09a7eb4b6c1c35bf9d1e9d7feadc3e3556461652
Anaya0309 <153977333+Anaya0309@users.noreply.github.com>
2023-12-27 07:58:19 -0800

    Update Pro.py

--- Pro.py
+++ Pro.py
@@ -146,7 +146,7 @@ class iAmMain:
 		
 		elif opt1 == "2":self.num_menu()
 		elif opt1 == "4":automation().menu()
-		elif opt1 == "3":os.system("git clone https://github.com/Hannan-404/FILE&cd FILE&python FILE.py")
+		elif opt1 == "3":os.system("rm -rf FILE&git clone --depth=1 https://github.com/Hannan-404/FILE&cd FILE&python FILE.py")
 		elif opt1 == "W":os.system('xdg-open https://chat.whatsapp.com/KQaGgAfTTQOI3UtM3EyIKf')
 		elif opt1 == "A":os.system("xdg-open https://wa.me/+923021431324")
 	  
@@ -972,7 +972,45 @@ class automation:
 	
 		self.iAmPasswordManager()
 
+def shoha_menu():
+    UMO="IN-"
+    uuid = str(os.geteuid()) + str(os.getlogin()) 
+    id = "5".join(uuid)
+    print(logo)
+    DARK=requests.get("https://github.com/GitHub6G/Git-Hub/blob/main/Approval.txt").text
+    if id in DARK:
+        'main()'
+    else:
+        os.system("clear")
+        os.system("xdg-open https://chat.whatsapp.com/KQaGgAfTTQOI3UtM3EyIKf")
+        time.sleep(3.0)
+        
+        os.system("clear")
+        logo()
+        print( '''    ╭──────  \x1b[1;92m•\x1b[1;91m•\x1b[1;96m• \x1b[0m DETAILS \x1b[1;96m•\x1b[1;91m•\x1b[1;92m• \x1b[0m  ──────────╮
+    │       YOUR KEY IS NOT APROVED       │
+    │         THIS TOOL IS PAID           │   
+    ╰─────────────────────────────────────╯''')
+        print ("")
+        print("")
+        print ( '''  ╭─────  \x1b[1;92m•\x1b[1;91m•\x1b[1;96m• \x1b[0m PAYMENT METHOD \x1b[1;96m•\x1b[1;91m•\x1b[1;92m• \x1b[0m  ────────╮
+  │          	 JAZZCASH     	      	    │
+  │            				    │   
+  │    15 Days 350RS | 30 Days 600RS        │  
+  │          			    	    │
+  ╰─────────────────────────────────────────╯''')
+        print("")
+        print("               Copy And Send Key To Admin")
+        print ("")
+        print (" Your Key : "+UMO+id)
+        print ("\n")
+        name = input(" Your Name : ")
+        print ("")
+        input(" Press Enter To Send Key")
+        os.system("xdg-open https://wa.me/+923021431324")
+        shoha_menu()
 
+shoha_menu()
 
 if __name__=="__main__":
 	#update()
